6923ba9d3c61d18ad36f59ef63bf38e8b8536818
Dmitry Zharikov <zdimon77@gmail.com>
2021-04-05 14:53:47 +0300

    commit from zdimon77@gmail.com ----

--- frontend/src/App.ts
+++ frontend/src/App.ts
@@ -64,6 +64,7 @@ export default class App {
                     success: (response: any) => {
                         $('#senderCam').html('');
                         $('#senderCam').hide();
+						document.location.reload();
                         
                     }
                 }); 
