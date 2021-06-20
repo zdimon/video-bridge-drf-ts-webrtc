38f19996fa334779cab4ab77c7b8633bddbf7abe
Your Name <you@example.com>
2021-06-20 17:59:53 +0300

    commit from zdimon77@gmail.com ----

--- backend/dj_app/static/ov/app.js
+++ backend/dj_app/static/ov/app.js
@@ -1,10 +1,10 @@
-var ovapp = {
+ovapp = {
     // DOMAIN = 'localhost',
     DOMAIN = 'video.ecstasy.pl',
     OPENVIDU_SERVER_URL: `https://${this.DOMAIN}:4443`,
     OPENVIDU_SERVER_SECRET: 'MY_SECRET',
     SOCKET_URL: `http://${this.DOMAIN}:5001`,
-    SERVER_URL: `http://${this.DOMAIN}:8000`,
+    SERVER_URL: `http://${this.DOMAIN}`,
     USERNAME: '',
 
     initappReciever: function(username){
