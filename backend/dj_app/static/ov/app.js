673ba2547c357064a494d99e98f24e3c07578c98
Your Name <you@example.com>
2021-06-20 18:19:00 +0300

    commit from zdimon77@gmail.com ----

--- backend/dj_app/static/ov/app.js
+++ backend/dj_app/static/ov/app.js
@@ -1,7 +1,7 @@
 ovapp = {
     // DOMAIN: 'localhost',
     DOMAIN: 'video.ecstasy.pl',
-    OPENVIDU_SERVER_URL: `https://video.ecstasy.pl/openvidu`,
+    OPENVIDU_SERVER_URL: `wss://video.ecstasy.pl/openvidu`,
     OPENVIDU_SERVER_SECRET: 'MY_SECRET',
     SOCKET_URL: `https://video.ecstasy.pl`,
     SERVER_URL: `https://video.ecstasy.pl`,
